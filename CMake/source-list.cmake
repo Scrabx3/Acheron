60b4340f4932fb571d2954f61cfd5a11b1d15c0f
Scrabx3 <kris.vlntn@gmail.com>
2023-03-10 23:24:49 +0100

    sourcelist

--- CMake/source-list.cmake
+++ CMake/source-list.cmake
@@ -1,4 +1,46 @@
 set(CXX_FILES
 	src/PCH.h
 	src/main.cpp
+
+	src/Random.h
+	src/Singleton.h
+	src/GameForms.h
+	src/Script.h
+	src/Settings.h
+	src/Settings.cpp
+
+	src/Acheron/Misc.h
+	src/Acheron/Misc.cpp
+	src/Acheron/Defeat.h
+	src/Acheron/Defeat.cpp
+	src/Acheron/EventSink.h
+	src/Acheron/EventSink.cpp
+	src/Acheron/Resolution.h
+	src/Acheron/Resolution.cpp
+	src/Acheron/Validation.h
+	src/Acheron/Validation.cpp
+
+	src/Acheron/Animation/Animation.h
+	src/Acheron/Animation/Animation.cpp
+
+	src/Acheron/Hooks/Hooks.h
+	src/Acheron/Hooks/Hooks.cpp
+	src/Acheron/Hooks/Processing.h
+	src/Acheron/Hooks/Processing.cpp
+	src/Acheron/Hooks/Console.h
+
+	src/Acheron/Interface/Interface.h
+	src/Acheron/Interface/HunterPride.h
+	src/Acheron/Interface/HunterPride.cpp
+
+	src/Papyrus/Config.h
+	src/Papyrus/Config.cpp
+	src/Papyrus/Functions.h
+	src/Papyrus/Functions.cpp
+	src/Papyrus/Events.h
+
+	src/Serialization/Serialize.h
+	src/Serialization/Serialize.cpp
+	src/Serialization/EventManager.h
+	src/Serialization/EventManager.cpp
 )
