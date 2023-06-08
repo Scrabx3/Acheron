cb27ebdd165a9575e04e307f01f5c040e7160d46
Scrabx3 <kris.vlntn@gmail.com>
2023-06-08 10:11:38 +0200

    overhauled racetype logic

--- src/Acheron/Animation/Animation.cpp
+++ src/Acheron/Animation/Animation.cpp
@@ -2,13 +2,73 @@
 
 namespace Acheron
 {
-	void Animation::PlayAnimation(RE::TESObjectREFR* a_reference, const char* a_animation)
+	std::string Animation::GetRaceType(RE::Actor* a_actor)
 	{
-		SKSE::GetTaskInterface()->AddTask([=]() { a_reference->NotifyAnimationGraph(a_animation); });
+		const auto base = a_actor->GetActorBase();
+		const auto race = a_actor->GetRace();
+		const auto sex = base->GetSex();
+		if (!race || !base || sex == RE::SEXES::kNone) {
+			logger::critical("Actor {:X} (Base: {:X}) has no race/base/sex", a_actor->GetFormID(), base ? base->GetFormID() : 0);
+			return "";
+		}
+		const std::string_view rootTMP{ race->rootBehaviorGraphNames[sex].data() };
+		const auto root{ rootTMP.substr(rootTMP.rfind('\\') + 1) };
+		if (root == "0_Master.hkx") {
+			return "Human";
+		}
+
+		const std::unordered_map<std::string_view, std::string> behaviorfiles{
+			{ "WolfBehavior.hkx", "Wolf"  },
+			{ "DogBehavior.hkx", "Dog"  },
+			{ "ChickenBehavior.hkx", "Chicken"  },
+			{ "HareBehavior.hkx", "Hare"  },
+			{ "AtronachFlameBehavior.hkx", "FlameAtronach"  },
+			{ "AtronachFrostBehavior.hkx", "FrostAtronach"  },
+			{ "AtronachStormBehavior.hkx", "StormAtronach"  },
+			{ "BearBehavior.hkx", "Bear"  },
+			{ "ChaurusBehavior.hkx", "Chaurus"  },
+			{ "H-CowBehavior.hkx", "Cow"  },
+			{ "DeerBehavior.hkx", "Deer"  },
+			{ "CHaurusFlyerBehavior.hkx", "ChaurusHunter"  },
+			{ "VampireBruteBehavior.hkx", "Gargoyle"  },
+			{ "BenthicLurkerBehavior.hkx", "Lurker"  },
+			{ "BoarBehavior.hkx", "Boar"  },
+			{ "BCBehavior.hkx", "DwarvenBallista"  },
+			{ "HMDaedra.hkx", "Seeker"  },
+			{ "NetchBehavior.hkx", "Netch"  },
+			{ "RieklingBehavior.hkx", "Riekling"  },
+			{ "ScribBehavior.hkx", "AshHopper"  },
+			{ "DragonBehavior.hkx", "Dragon"  },
+			{ "Dragon_Priest.hkx", "DragonPriest"  },
+			{ "DraugrBehavior", "Draugr"  },
+			{ "SCBehavior.hkx", "DwarvenSphere"  },
+			{ "DwarvenSpiderBehavior.hkx", "DwarvenSpider"  },
+			{ "SteamBehavior.hkx", "DwarvenCenturion"  },
+			{ "FalmerBehavior.hkx", "Falmer"  },
+			{ "FrostBiteSpiderBehavior.hkx", "Spider"  },
+			{ "GiantBehavior.hkx", "Giant"  },
+			{ "GoatBehavior.hkx", "Goat"  },
+			{ "HavgravenBehavior.hkx", "Hagraven"  },
+			{ "HorkerBehavior.hkx", "Horker"  },
+			{ "HorseBehavior.hkx", "Horse"  },
+			{ "IceWraithBehavior.hkx", "IceWraith"  },
+			{ "MammothBehavior.hkx", "Mammoth"  },
+			{ "MudcrabBehavior.hkx", "Mudcrab"  },
+			{ "SabreCatBehavior.hkx", "Sabrecat"  },
+			{ "SkeeverBehavior.hkx", "Skeever"  },
+			{ "SlaughterfishBehavior.hkx", "Slaughterfish"  },
+			{ "SprigganBehavior.hkx", "Spriggan"  },
+			{ "TrollBehavior.hkx", "Troll"  },
+			{ "VampireLord.hkx", "VampireLord"  },
+			{ "WerewolfBehavior.hkx", "Werewolf"  },
+			{ "WispBehavior.hkx", "Wispmother"  },
+			{ "WitchlightBehavior.hkx", "Wisp"  },
+		};
+		auto where = behaviorfiles.find(root);
+		return where == behaviorfiles.end() ? "" : where->second;
 	}
 
-	// TODO: get racetype based on skeleton data?
-	std::string Animation::GetRaceType(RE::Actor* subject)
+	std::string Animation::GetRaceTypeLegacy(RE::Actor* subject)
 	{
 		if (IsNPC(subject))
 			return "Human"s;
--- src/Acheron/Animation/Animation.h
+++ src/Acheron/Animation/Animation.h
@@ -2,6 +2,7 @@
 
 namespace Acheron::Animation
 {
-	void PlayAnimation(RE::TESObjectREFR* a_reference, const char* a_animation);								 // Wrapper for sending thread save animation events cuz Im lazy
-	std::string GetRaceType(RE::Actor* a_actor);																								 // RaceType definition to get struggle motion
+	std::string GetRaceType(RE::Actor* a_actor);
+
+	std::string GetRaceTypeLegacy(RE::Actor* a_actor);
 }
