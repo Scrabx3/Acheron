5adb1bbf6a02f920b40173d9660b681240dce39f
Scrabx3 <kris.vlntn@gmail.com>
2024-05-30 15:59:23 +0200

    add Random::shuffle

--- src/Random.h
+++ src/Random.h
@@ -12,5 +12,12 @@ namespace Random
 			return std::uniform_int_distribution<T>{ a_min, a_max }(eng);
 		else
 			return std::uniform_real_distribution<T>{ a_min, a_max }(eng);
-	}	
+	}
+
+	template <class V>
+	void shuffle(V& a_container)
+	{
+		static std::mt19937 eng{ std::random_device{}() };
+		std::ranges::shuffle(a_container, eng);
+	}
 };
