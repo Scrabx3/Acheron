1252918aff22c7b5a2efe8da9e35139daef63b52
Scrabx3 <kris.vlntn@gmail.com>
2025-03-06 20:10:09 +0100

    bump version to 1.6.0

--- xmake.lua
+++ xmake.lua
@@ -5,7 +5,7 @@ PROJECT_NAME = "Acheron"
 
 -- Project
 set_project(PROJECT_NAME)
-set_version("1.5.4")
+set_version("1.6.0")
 set_languages("cxx23")
 set_license("apache-2.0")
 set_warnings("allextra", "error")
